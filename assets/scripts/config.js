1a372c91a79478fad9033c140ae1ae5222ddeabd
Kane <Pperson599@gmail.com>
2020-04-06 10:34:48 -0400

    Changed config for deployed

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://cherry-crisp-90830.herokuapp.com',
+  production: 'https://virtuoso-api.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
